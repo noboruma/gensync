6f09387b8cb3bfdae01b7f7d43bb24b8b381830f
Thomas Legris <legris.thomas@gmail.com>
2026-06-19 09:44:47 +0900

    add missing compareAnd methods

--- map.go
+++ map.go
@@ -27,6 +27,14 @@ func (m *Map[K, V]) Delete(key K) {
 	m.m.Delete(key)
 }
 
+func (m *Map[K, V]) CompareAndDelete(key K, value V) bool {
+	return m.m.CompareAndDelete(key, value)
+}
+
+func (m *Map[K, V]) CompareAndSwap(key K, old, new V) bool {
+	return m.m.CompareAndSwap(key, old, new)
+}
+
 func (m *Map[K, V]) LoadOrStore(key K, value V) (V, bool) {
 	actual, loaded := m.m.LoadOrStore(key, value)
 	return actual.(V), loaded
